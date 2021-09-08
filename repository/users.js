71ba136868326871d8e9122dbec68e7f49e060f9
Sharoz Ijaz <sherry@sharoz.dev>
2021-09-08 15:18:45 +0500

    added delte func

--- repository/users.js
+++ repository/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 class UserRepository {
   constructor(filename) {
     if (!filename) {
@@ -19,9 +20,28 @@ class UserRepository {
     );
   }
   async create(attrs) {
+    attrs.id = this.randomId();
     const records = await this.getAll();
     records.push(attrs);
-    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    await this.writeAll(records);
+  }
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((records) => records.id === id);
+  }
+  async delete(id) {
+    const records = await this.getAll();
+    return (filterRecords = records.filter((record) => record.id !== id));
+    await this.writeAll(filterRecords);
   }
 }
 const repo = new UserRepository("users.json");
